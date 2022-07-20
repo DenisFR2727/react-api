548e70e109b0019881e02fbc4e230dba0d8a19a5
DenisFR2727 <81112453+DenisFR2727@users.noreply.github.com>
2022-07-21 00:29:42 +0300

    Create Menu.js

---
+++ src/components/Menu.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Buttons from "./Buttons/Buttons";
+
+
+const Menu =()=>{
+
+
+    return(
+         <section>
+             <div className="rectangle"></div>
+             <div className="Menu">
+                 <div className="logo"></div>
+                 <Buttons />
+             </div>
+         </section>
+    )
+}
+export default Menu;
