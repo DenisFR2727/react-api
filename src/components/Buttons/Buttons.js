6d7b9d0de83c009929911f4bdbd48f8c9ae26a0f
DenisFR2727 <81112453+DenisFR2727@users.noreply.github.com>
2022-07-21 00:29:26 +0300

    Create Buttons.js

---
+++ src/components/Buttons/Buttons.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./buttons.css"
+
+const Buttons =()=>{
+
+
+
+    return(
+         <div className="button_2">
+           <button type="submit" className="button_menu1">Users</button>
+           <button type="submit" className="button_menu2">Sign up</button>
+         </div>
+        
+         
+    )
+}
+export default Buttons;
